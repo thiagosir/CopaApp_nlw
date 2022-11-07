e3d9b100ad4867fe875da5153abcf20005d792d7
thiagosir <thiagobento9@hotmail.com>
2022-11-07 13:42:32 -0300

    Iniciando com o tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
+      colors: {
+        gray: {
+          900: "#121214",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
