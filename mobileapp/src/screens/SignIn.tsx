eb05eba1419ad7515abd37cbe118bae612e33788
thiagosir <thiagobento9@hotmail.com>
2022-11-23 14:29:39 -0300

    Iniciando customizacao do Mobileapp

--- mobileapp/src/screens/SignIn.tsx
+++ mobileapp/src/screens/SignIn.tsx
@@ -1,11 +1,19 @@
-import { NativeBaseProvider, Text, Center } from "native-base";
+import { Text, Center, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import Logo from "../assets/logo.svg";
+
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>
-        SignIn
-      </Text>
+        <Logo width={212} height={40} />
+
+        <Button 
+        title="Entrar com Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        />
     </Center>
   );
 }
